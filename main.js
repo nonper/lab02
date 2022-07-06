5e2f9a360675c8844f34188a521302482a893986
biggib002 <supachok_ji@cmu.ac.th>
2022-07-06 16:53:59 +0700

    3.5

--- main.js
+++ main.js
@@ -2,15 +2,16 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Shoes',
-            image: './assets/images/socks_green.jpg',
-            inStock: false,
+            brand: 'SE 331',
             inventory: 100,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' }
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 }
             ],
-            cart: 0
+            selectedVariant: 0,
+            cart: 0,
+            onSale: true,
 
         }
     },
@@ -20,6 +21,23 @@ const app = Vue.createApp({
         },
         updateImage(variantImage) {
             this.image = variantImage
+        },
+        updateVariant(index){
+            this.selectedVariant = index;
         }
+    },
+    computed: {
+        title() {
+            if(!this.onSale){
+                return this.brand + ' ' + this.product
+            }
+            return this.brand + ' ' + this.product + ' is on sale.'
+        },
+        image(){
+            return this.variants[this.selectedVariant].image
+        },
+        inStock(){
+            return this.variants[this.selectedVariant].quantity
+        },
     }
 })
